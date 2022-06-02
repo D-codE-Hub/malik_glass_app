6d9fbd49aed77ea09f8f2ca0ccde325f5183fc6b
D-codE-Hub <mailtodecode@gmail.com>
2022-06-02 11:17:41 +0530

    Min running mtr and sqr update

--- malik_glass_app/public/js/quotation.js
+++ malik_glass_app/public/js/quotation.js
@@ -36,14 +36,22 @@ let set_datas = function(frm, cdt, cdn) {
 		var height = d.height/100;
 		var width = d.width/100;
 	}
+	var per_sqm =  height*width
+	var running_meter = ((d.side_h*height)+(d.side_w*width))*d.quantity_of_glass
+	if (per_sqm < 0.5){
+		per_sqm = 0.5;
+	}
+	if (running_meter < 1){
+		running_meter = 1;
+	}
 	// console.log(height*width)
 	frappe.model.set_value(d.doctype, d.name, "qty",1);
 
-	frappe.model.set_value(d.doctype, d.name, "per_sqm",  height*width);
+	frappe.model.set_value(d.doctype, d.name, "per_sqm", per_sqm);
 	frappe.model.set_value(d.doctype, d.name, "total_sqm",  d.per_sqm*d.quantity_of_glass );
 	frappe.model.set_value(d.doctype, d.name, "qty",  d.total_sqm);
 
-	frappe.model.set_value(d.doctype, d.name, "running_meter",  ((d.side_h*height)+(d.side_w*width))*d.quantity_of_glass);
+	frappe.model.set_value(d.doctype, d.name, "running_meter", running_meter);
 	frm.set_value("total_running_mtr", 0);
 	frm.set_value("total_sqm", 0);
 	for (let row of frm.doc.items) {
